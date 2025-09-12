e5b605b0c113811aa97998859c374ce7585066c3
OhanaSmile <79485957+OhanaSmile@users.noreply.github.com>
2025-09-12 13:18:27 +0000

    軽微な修正

--- app.py
+++ app.py
@@ -13,8 +13,16 @@
 ##---------------------------------
 
 import streamlit as st
-from scipy.stats import binomtest
+from scipy.stats import binomtest, binom
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import matplotlib as mpl
+
+# font_path = "fonts/NotoSansJP-Regular.otf"
+# fm.fontManager.addfont(font_path)
+# mpl.rcParams["font.family"] = fm.FontProperties(fname=font_path).get_name()
+
 
 st.title('1×2 母比率検定 早見アプリ')
 
@@ -35,5 +43,36 @@ if st.button('p値を計算'):
     st.subheader('計算結果')
     st.write(f'**N={N}, 成功数={k}の場合の片側p値：**')
     st.metric(label='p値', value=f'{np.round(p_value, 3)}')
+
+    # グラフ表示
+    x = np.arange(0, N + 1)
+    pmf = binom.pmf(x, N, 0.5)
+
+    fig, ax = plt.subplots(figsize=(8, 4))
+    bars = ax.bar(x, pmf, color='lightblue' ,align='center') 
+
+    for i, b in enumerate(bars):
+        if i >= k:
+            b.set_color('orange')
+
+    ax.axvline(k, color='red', linestyle='--', label='成功数の位置')
+    ax.text(k, max(pmf)*0.9, f'成功数={k}', color='red', fontsize=10, ha='center', va='top')
+    # 軸・タイトル
+    ax.set_xlabel('成功数 x')
+    ax.set_ylabel('P(X = x | N, p=0.5)')
+    ax.set_title('二項分布 PMF（H0: p=0.5）と片側領域（x ≥ k）')
+    
+    # p値を図中に表示
+    ax.text(
+        0.98, 0.95,
+        f'p値（x ≥ {k}）= {p_value:.3f}',
+        transform=ax.transAxes, ha='right', va='top',
+        bbox=dict(boxstyle='round', alpha=0.2)
+    )
+
+    st.pyplot(fig)
+
+
+
 
 
